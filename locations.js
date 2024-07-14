f5cd539ca24c33e50b27677acff1a7064052cf5e
CLlauger <50671982+CLlauger@users.noreply.github.com>
2024-07-14 18:40:17 +0200

    Addició d'informació i canvi en les imatges

--- locations.js
+++ locations.js
@@ -37,7 +37,8 @@ const locations = [
 			"<h2>Creu dels carlins de Vilanna</h2>",
 			"<small>Divisió administrativa: Vilanna; Bescanó; Gironès; Comarques gironines; Catalunya</small><br/><br/>",
 			"<img style='width: 300px; display: block; margin: auto;' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Creu_dels_carlins_de_Vilanna_%28vista_de_migjorn%29.jpg/320px-Creu_dels_carlins_de_Vilanna_%28vista_de_migjorn%29.jpg'/><br/>",
-			"Creu de terme al poble de Vilanna, al terme municipal de Bescanó. Construïda el 1913."
+			"Creu de terme al poble de Vilanna, al terme municipal de Bescanó. Instal·lada el 14 de setembre de 1913.",
+			"<br/><a href='#creu_dels_carlins_de_vilanna'>Veure més informació</a>"
 		],
 		visited: true
 	},
